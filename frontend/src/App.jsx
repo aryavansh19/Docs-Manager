991c405dc21bec4663deda9c345a416af49d7626
Aryavansh Saini <aryavansh.120.com>
2025-12-25 22:53:50 +0530

    v2.2 - Added UI and Login System

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import FloatingNav from "./components/FloatingNav";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Verification from "./pages/Verification";
+import Dashboard from "./pages/Dashboard";
+import AuthOptions from "./pages/AuthOptions";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <>
+      <FloatingNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthOptions />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/verify" element={<Verification />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* Redirect any other path to Home */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
